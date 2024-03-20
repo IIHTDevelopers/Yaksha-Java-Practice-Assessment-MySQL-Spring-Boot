6e667703c8beddc0f3b0a2ece8c32b838dd4c7f7
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-03-20 16:39:48 +0530

    Add files via upload

---
+++ src/main/java/com/practice/practice/assessment/PracticeAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.practice.practice.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeAssessmentApplication.class, args);
+	}
+
+}
